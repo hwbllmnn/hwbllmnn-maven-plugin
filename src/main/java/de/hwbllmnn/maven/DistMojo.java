41ff5ef78310ca69f0a8b1e5873ee806b782d940
Andreas Schmitz <vstranger@arcor.de>
2010-12-12 21:30:12 +0100

    first working version

--- src/main/java/de/hwbllmnn/maven/DistMojo.java
+++ src/main/java/de/hwbllmnn/maven/DistMojo.java
@@ -4,9 +4,14 @@
  */
 package de.hwbllmnn.maven;
 
+import static org.codehaus.plexus.util.FileUtils.copyFile;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -14,75 +19,49 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 
 /**
- * @goal copy
- *
+ * @goal dist
+ * 
  * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
  * @author last edited by: $Author: aschmitz $
- *
+ * 
  * @version $Revision: 28721 $, $Date: 2010-12-10 15:23:52 +0100 (Fri, 10 Dec 2010) $
  */
 public class DistMojo extends AbstractMojo {
 
-    /**
-     * @parameter default-value="${project}"
-     * @required
-     * @readonly
-     */
-    private MavenProject project;
+	/**
+	 * @parameter default-value="${project}"
+	 * @required
+	 * @readonly
+	 */
+	private MavenProject project;
 
-    /**
-     * @parameter
-     */
-    private Copy[] files;
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		Log log = getLog();
+		File basedir = project.getBasedir();
+		File target = new File(basedir, "target/dist");
 
-    public void execute()
-                            throws MojoExecutionException, MojoFailureException {
-        Log log = getLog();
-        if ( files == null ) {
-            log.debug( "No files configured." );
-            return;
-        }
+		if (!target.mkdirs()) {
+			log.warn("Could not create target directory: " + target);
+		}
 
-        File basedir = project.getBasedir();
-        for ( Copy copy : files ) {
-            log.info( "Copy " + copy.from + " to " + copy.to );
-            File from = new File( basedir, copy.from );
-            File to = new File( basedir, copy.to );
-            if ( !to.getParentFile().mkdirs() ) {
-                log.warn( "Could not create parent directories for " + to + "." );
-                continue;
-            }
-//            try {
-//                copyFile( from, to );
-//            } catch ( IOException e ) {
-//                log.warn( "Could not copy " + copy.from + " to " + copy.to + ": " + e.getLocalizedMessage() );
-//                log.debug( e );
-//            }
-        }
-    }
+		List<Artifact> artifacts = new LinkedList<Artifact>();
+		for (Object o : project.getCollectedProjects()) {
+			MavenProject module = (MavenProject) o;
+			artifacts.addAll(module.getAttachedArtifacts());
+			artifacts.add(module.getArtifact());
+		}
 
-    /**
-     *
-     * @author <a href="mailto:schmitz@lat-lon.de">Andreas Schmitz</a>
-     * @author last edited by: $Author: aschmitz $
-     *
-     * @version $Revision: 28721 $, $Date: 2010-12-10 15:23:52 +0100 (Fri, 10 Dec 2010) $
-     */
-    public static class Copy {
-        /**
-         * @parameter
-         */
-        String from;
+		log.info("Collected artifacts: " + artifacts);
 
-        /**
-         * @parameter
-         */
-        String to;
-
-        @Override
-        public String toString() {
-            return from + " -> " + to;
-        }
-    }
+		for (Artifact a : artifacts) {
+			File file = a.getFile();
+			try {
+				copyFile(file, new File(target, file.getName()));
+				log.info("Copied artifact " + file.getName());
+			} catch (IOException e) {
+				log.warn("Could not copy artifact: " + file);
+			}
+		}
+	}
 
 }
